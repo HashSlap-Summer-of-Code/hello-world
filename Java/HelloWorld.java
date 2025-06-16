dff7acb01eb8b6e32024e14712049b17e5abcb1d
samarthkoli <154528119+SamarthKoli@users.noreply.github.com>
2025-06-16 13:33:23 +0530

    Added Hello World in Java

---
+++ Java/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+
+    //Main method 
+    public static void main(String[] args) {
+        //Line to print Hello world in Java
+        System.out.println("Hello, World!");
+    }
+}
