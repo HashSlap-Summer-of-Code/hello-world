38f76e64498c986f12a6fd18f358d53dbd92e703
sneha <snehapatne2311@gmail.com>
2025-06-18 16:37:00 +0530

    Added Hello World in C#

---
+++ programs/csharp/Hello.cs
@@ -0,0 +1,11 @@
+// Compile and run: csc Hello.cs && ./Hello (on Windows: Hello.exe)
+
+using System;
+
+class HelloWorld
+{
+    static void Main()
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
